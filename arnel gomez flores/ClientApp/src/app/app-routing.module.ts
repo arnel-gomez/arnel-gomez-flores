fe1eb93032d095a8408fa56b3aad08ece3624459
Arnel Gomez <devops@icemexico.com>
2023-11-06 17:32:56 -0600

    Cambios

--- arnel gomez flores/ClientApp/src/app/app-routing.module.ts
+++ arnel gomez flores/ClientApp/src/app/app-routing.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./pages/login/login.component";
 import { TablesComponent } from './pages/tables/tables.component';
-import { ArticuloComponent } from './pages/articulo/tables.component';
+import { ArticuloComponent } from './pages/articulo/articulo.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'tiendas', component: TablesComponent },
-  { path: 'tiendas', component: TiendaComponent },
-  { path: 'tiendas', component: TiendaComponent },
+  { path: 'aarticulos', component: ArticuloComponent },
 ];
 
 @NgModule({
